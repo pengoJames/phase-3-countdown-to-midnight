15a5b0ce7e024c253f79ec29b1afa4524bda8006
Pe-ngo <jamespengo95@gmail.com>
2022-08-24 17:10:41 +0300

    Completed Assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(number)
+
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+countdown(12)
+
+
+def countdown_with_sleep(num)
+
+    while num > 0
+        sleep 1
+        puts "#{num} Hello!"
+        num -= 1
+    end
+end
+countdown_with_sleep(12)
